d72d5a324df487484c128b3df04f7347b76a6402
KazutomoKita <elliottstar77@yahoo.co.jp>
2020-03-02 15:54:03 +0900

    ファーストコミット

---
+++ git_test/git_test/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  git_test
+//
+//  Created by Kazutomo Kita on 2020/03/02.
+//  Copyright © 2020 Kazutomo Kita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        label.text = "おちんちん"
+    }
+
+
+}
+
